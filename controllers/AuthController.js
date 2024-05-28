303d35f63e8c8eb73911a49bd75a73ec39a26965
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-05-28 17:22:03 -0300

    Finalizado o método registerUser

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -8,7 +8,7 @@ module.exports = class AuthController {
   static register(req, res) {
     res.render("auth/register");
   }
-  static async registerPost(req, res) {
+  static async registerUser(req, res) {
     const { name, email, password, confirmpassword } = req.body;
 
     // password match validation
@@ -16,6 +16,36 @@ module.exports = class AuthController {
       req.flash("message", "As senhas não conferem, tente novamente!");
       res.render("auth/register");
       return;
+    }
+
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
+
+    if (checkIfUserExists) {
+      req.flash("message", "O e-mail já está cadastrado!");
+      res.render("auth/register");
+      return;
+    }
+
+    //create a password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const createdUser = await User.create(user);
+      //initialize session
+      req.session.userid = createdUser.id;
+      req.flash("message", "Cadastro realizado com sucesso!");
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (err) {
+      console.log(err);
     }
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,6 @@ const AuthController = require("../controllers/AuthController");
 
 router.get("/login", AuthController.login);
 router.get("/register", AuthController.register);
-router.post("/register", AuthController.registerPost);
+router.post("/register", AuthController.registerUser);
 
 module.exports = router;
