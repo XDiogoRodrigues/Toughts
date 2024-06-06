0b44300ee384b4336f13984fc0e510bacf829b13
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-06-05 21:56:32 -0300

    Projeto finalizado

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,27 +7,33 @@ module.exports = class AuthController {
 
   static async loginUser(req, res) {
     const { email, password } = req.body;
+
     // find user
     const user = await User.findOne({ where: { email: email } });
 
     if (!user) {
-      req.flash("message", "Usuário não encontrado!");
-      res.render("auth/login");
+      res.render("auth/login", {
+        message: "Usuário não encontrado!",
+      });
+
       return;
     }
 
-    // check if passwords match
-
+    // compare password
     const passwordMatch = bcrypt.compareSync(password, user.password);
 
     if (!passwordMatch) {
-      req.flash("message", "Senha inválida!");
+      req.flash("message", "Senha incorreta!");
       res.render("auth/login");
+
+      return;
     }
 
-    // initialize session
+    // auth user
     req.session.userid = user.id;
-    req.flash("message", "Autenticação realizada com sucesso!");
+
+    req.flash("message", "Login realizado com sucesso!");
+
     req.session.save(() => {
       res.redirect("/");
     });
